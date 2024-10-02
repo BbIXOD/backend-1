07ccdba1bf9547c9149a0d4d301d51868346d066
Maksym Diachok <maksdyachok2005@gmail.com>
2024-10-02 21:47:51 +0300

    Modify site structure.
    - home endpoint now just contains simple html
    - healthcheck endpoint with json output added

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
 from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
+def home():
+    return f"<p>Check my health!</p>", 200
+
+@app.route("/healthcheck")
 def healthcheck():
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return f"<p>Current time: {current_time}</p>"
+    response = {
+        "date": current_time,
+        "status": "running"
+    }
+    return jsonify(response), 200
